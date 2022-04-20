baf751fe9c4277fb09470875d5c33fdb9ba55fb6
Zhenhang Lu <lzh18081579@mail.ustc.edu.cn>
2022-04-20 17:52:55 +0800

    fix bugs of the outspacing

--- bin/resampling.py
+++ bin/resampling.py
@@ -30,7 +30,8 @@ def generate_new_tomo(resample_tomo, out_name):
 def resample_image(tomo, pixel_size, outspacing=17.142):
 
     tomo_sitk = sitk.ReadImage(tomo)
-    out_spacing = prepare_resample(tomo, pixel_size, outspacing)
+    #out_spacing = prepare_resample(tomo, pixel_size, outspacing)
+    out_spacing = [outspacing, outspacing, outspacing]
     original_spacing = tomo_sitk.GetSpacing()
     original_size = tomo_sitk.GetSize()
     out_size = [
@@ -62,14 +63,15 @@ def measure(tomo, pixel_size, outspacing=17.142):
     else:
         original_spacing = [pixel_size, pixel_size, pixel_size]
     original_size = sitk_tomo.GetSize()
-    output_spacing = prepare_resample(tomo, pixel_size, outspacing)
+    #output_spacing = prepare_resample(tomo, pixel_size, outspacing)
+    output_spacing = [outspacing, outspacing, outspacing]
     out_size = [
         int(np.round(original_size[0] * original_spacing[0] / output_spacing[0])),
         int(np.round(original_size[1] * original_spacing[1] / output_spacing[1])),
         int(np.round(original_size[2] * original_spacing[2] / output_spacing[2]))
     ]
-    out_spacing = [outspacing, outspacing, outspacing]
-    return [original_spacing, original_size, out_spacing, out_size]
+    #out_spacing = [outspacing, outspacing, outspacing]
+    return [original_spacing, original_size, output_spacing, out_size]
 
 
 if __name__ == '__main__':  
