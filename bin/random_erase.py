714ec966e75e547e3aa58e98c18f36561c040379
ZhenhangLu <lzh18081579@mail.ustc.edu.cn>
2024-04-26 17:49:27 +0800

    reshape code

--- bin/random_erase.py
+++ bin/random_erase.py
@@ -1,71 +1,64 @@
-#! usr/bin/env python3
-# random erasing in .mrc data, generate N blocks in image, in order to train a robuster model
+#!/usr/bin/env python3
+# random erasing in tomogram, generate N blocks in image, in order to train a robuster model
 # use mean value instead of random value
 
-# 3d random erasing plays different role with 2d one, 2d simulate low-quality image while 3d simulate omega vesciles and others similar
-# 
 
+import fire
 import mrcfile
 import numpy as np
-import argparse
-
-N1 = 40 # N blocks in image(for 2d)
-N2 = 120 # N blocks (for 3d)
 
 
-def random_erasing(img, M):
+
+def random_erasing(img, M, N1):
 	width = img.shape[0]
 	height = img.shape[1]
 
+	square = 15
 	for attempt in range(N1):
-		se = 15*15 #15*15 in bin8 files
-		a = int(np.round(np.sqrt(se)))
-		if a < min(width, height):
-			x1 = np.random.randint(0, width - a)
-			y1 = np.random.randint(0, height - a)
-			img[x1: x1+a, y1: y1+a] = 0
+		x1 = np.random.randint(0, width - square)
+		y1 = np.random.randint(0, height - square)
+		img[x1: x1+square, y1: y1+square] = M
 	return img
 
 
-def random_erasing_3d(img, M):
+def random_erasing_3d(img, M, N2):
 	width, height, layer = img.shape[0], img.shape[1], img.shape[2]
 
+	cube = 10
 	for attempt in range(N2):
-		se = 10*10
-		a = int(np.round(np.sqrt(se)))
-		if a < min(width, height, layer):
-			x1 = np.random.randint(0, width-a)
-			y1 = np.random.randint(0, height-a)
-			z1 = np.random.randint(0, layer-a)
-			img[x1:x1+a, y1:y1+a, z1:z1+a] = 0
+		x1 = np.random.randint(0, width-cube)
+		y1 = np.random.randint(0, height-cube)
+		z1 = np.random.randint(0, layer-cube)
+		img[x1:x1+cube, y1:y1+cube, z1:z1+cube] = M
 	return img
+
+
+def apply(mrc, N1=40, N2=120, output=None):
+	'''
+	param N1: number of 2d blocks in image with size 15*15
+	param N2: number of 3d blocks in image with size 10*10*10
+	'''
+
+	with mrcfile.open(mrc) as mrc_file:
+		data = mrc_file.data
+		data.flags.writeable = True
+		data = data.transpose(2, 1, 0)
+	mean = np.mean(data)
+	mrc_random_erased = np.zeros_like(data)
+
+	for i in range(data.shape[2]):
+		mrc_random_erased[:, :, i] = random_erasing(data[:, :, i], mean, N1)
+	mrc_random_erased = random_erasing_3d(mrc_random_erased, mean, N2)
+
+	mrc_random_erased = mrc_random_erased.transpose(2, 1, 0)
+
+	if output is None:
+		output = mrc.replace('.mrc', '_era'+str(N1)+'-'+str(N2)+'.mrc')
+	with mrcfile.new(output, overwrite=True) as mrc_file:
+		mrc_file.set_data(mrc_random_erased.astype(np.float32))
+	
+	print('random erasing done, output:', output)
 
 
 if __name__ == '__main__':
-
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('--tomo_file', type=str, default=None, help='Your original tomo')
-	parser.add_argument('--result_tomo', type=str, default=None, help='result tomo')
-	args = parser.parse_args()
-
-	with mrcfile.open(args.tomo_file) as s:
-		image = s.data # axis: z, y, x
-		image.flags.writeable = True
-		image = image.transpose(2, 1, 0) # (x, y, z)
-	mean = image.mean() # use mean value instead of random value
-	image_ran_era = np.zeros(image.shape)
-
-	for i in range(image.shape[2]):
-		image_ran_era[:, :, i] = random_erasing(image[:, :, i], M = mean)
-	
-	image_ran_era = random_erasing_3d(image_ran_era, M = mean)
-
-	image_ran_era = image_ran_era.transpose(2, 1, 0)
-
-	with mrcfile.new(args.result_tomo, overwrite=True) as m:
-		m.set_data(image_ran_era.astype(np.float32))
-    
-	print("already erasing")
-    
-    
-    
+	fire.Fire(apply)
