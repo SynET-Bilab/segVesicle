3d53efe75f7b5a80b685ddfc9210c167083d5074
liushuo <qq624753058@gmail.com>
2024-06-21 09:47:33 +0800

    add red center cross for visualization

---
+++ bin/center_cross.py
@@ -0,0 +1,38 @@
+from napari import Viewer
+from napari.utils.action_manager import action_manager
+from napari.utils.notifications import show_info
+import numpy as np
+import napari
+from napari.components.viewer_model import ViewerModel
+
+def center_cross_on_mouse(viewer_model: ViewerModel):
+    """move the cross to the mouse position"""
+
+    if not getattr(viewer_model, "mouse_over_canvas", True):
+        # There is no way for napari 0.4.15 to check if mouse is over sending canvas.
+        show_info(
+            "Mouse is not over the canvas. You may need to click on the canvas."
+        )
+        return
+
+    viewer_model.dims.current_step = tuple(
+        np.round(
+            [
+                max(min_, min(p, max_)) / step
+                for p, (min_, max_, step) in zip(
+                    viewer_model.cursor.position, viewer_model.dims.range
+                )
+            ]
+        ).astype(int)
+    )
+
+action_manager.register_action(
+    name='napari:move_point',
+    command=center_cross_on_mouse,
+    description='Move dims point to mouse position',
+    keymapprovider=ViewerModel,
+)
+
+action_manager.bind_shortcut('napari:move_point', 'C')
+
+action_manager._debug(True)
