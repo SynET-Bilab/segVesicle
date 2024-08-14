f053594e5260630a78ed16e8b0a04eb9b7f6f0b2
ZhenhangLu <lzh18081579@mail.ustc.edu.cn>
2024-08-14 15:14:19 +0800

    reshape code

--- bin/util/resample.py
+++ bin/util/resample.py
@@ -1,27 +1,26 @@
-import SimpleITK as sitk
-import numpy as np
+import mrcfile
+
 from scipy.ndimage import zoom
 
 
-def resample_image(tomo, pixel_size, outspacing=17.142):
-    # 使用 SimpleITK 读取图像
-    tomo_sitk = sitk.ReadImage(tomo)
+def get_tomo(input_data):
+    with mrcfile.open(input_data) as m:
+        data = m.data
+    return data
 
+
+def resample_image(tomo, pixel_size, out_name=None, outspacing=17.142):
+
+    original_data = get_tomo(tomo)
+    
     original_spacing = [pixel_size, pixel_size, pixel_size]
     out_spacing = [outspacing, outspacing, outspacing]
-    
-    # 获取原始图像数据
-    original_data = sitk.GetArrayFromImage(tomo_sitk)
-    
-    # 计算缩放因子
     scale_factors = [
         original_spacing[0] / out_spacing[0],
         original_spacing[1] / out_spacing[1],
         original_spacing[2] / out_spacing[2]
     ]
-    
-    # 使用 scipy.ndimage.zoom 进行重采样
+    # resample
     out_data = zoom(original_data, zoom=scale_factors, order=1)
-    
 
     return out_data
