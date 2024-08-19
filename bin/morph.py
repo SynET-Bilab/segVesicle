2f025854aa2868f3802b1700c780d4e078c91360
lzyustc <547108141@qq.com>
2024-08-19 10:31:03 +0800

    fit bugs

--- bin/morph.py
+++ bin/morph.py
@@ -229,6 +229,8 @@ def template(radii, center, evecs, shape, d=3):
         if ellip[i][0]<cube_ellip.shape[0] and ellip[i][1]<cube_ellip.shape[0] and ellip[i][2]<cube_ellip.shape[0]:
             ellip_.append(ellip[i])
     ellip_ = np.array(ellip_)
+    if len(ellip_)<5:
+        return cube_ellip
     cube_ellip[ellip_[:,0],ellip_[:,1],ellip_[:,2]] = 1
     cube_ellip=closing(cube_ellip,cube(d))
     circle = dilation(cube_ellip,cube(d)) - erosion(cube_ellip,cube(d))
